87c5f9e33bb877301fcda4af8cb658599c6b5740
Hygon <hygon806@gmail.com>
2022-04-09 21:02:39 +0200

    Connect to discord

--- src/main/java/fr/amgone/ecbot/Main.java
+++ src/main/java/fr/amgone/ecbot/Main.java
@@ -1,9 +1,23 @@
 package fr.amgone.ecbot;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
 import java.nio.file.Paths;
 
 public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
         Config config = new Config(Paths.get("config.toml"));
+
+        logger.info("Connecting to discord...");
+        DiscordApi discordApi = new DiscordApiBuilder()
+                .setToken(config.getDiscordToken())
+                .login().join();
+        logger.info("Done!");
+
     }
 }
